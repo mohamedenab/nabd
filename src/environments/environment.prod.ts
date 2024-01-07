576ddf0228b7b22b48bcf091ef152a5fb660d7ca
mohamed <moahemd3nab@gmail.com>
2024-01-07 15:04:27 +0200

    update proxy.conf.prod.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   apiUrl: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1',
-  auth: 'api/v1/auth',
-  locations: 'api/v1/locations',
-  report: 'api/v1/report',
-  users: 'api/v1/users',
-  specialization: 'api/v1/specialization',
-  patient: 'api/v1/patient',
-  medicine: 'api/v1/medicine',
-  history: 'api/v1/history',
-  upload: 'api/v1/upload'
+  auth: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/auth',
+  locations: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/locations',
+  report: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/report',
+  users: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/users',
+  specialization: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/specialization',
+  patient: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/patient',
+  medicine: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/medicine',
+  history: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/history',
+  upload: 'https://nabd-back-q3ux3.ondigitalocean.app/api/v1/upload'
 };
