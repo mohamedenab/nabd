6df99f10bdeecc04eeb31bf4344ad4ac73244f03
mohamed <moahemd3nab@gmail.com>
2024-04-28 01:04:11 +0300

    update env

---
+++ src/app/modules/locations/location-patients-management/location-patients-management.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PatientsManagementComponent } from './patients-management.component';
+
+describe('RegionComponent', () => {
+  let component: PatientsManagementComponent;
+  let fixture: ComponentFixture<PatientsManagementComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PatientsManagementComponent]
+    });
+    fixture = TestBed.createComponent(PatientsManagementComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
