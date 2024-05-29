14f5ffcda623e265c1d2feacdf46b8b0c81f89a7
amr mahmoud <94997694+AmrMahmoudSaid@users.noreply.github.com>
2024-05-29 04:10:09 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://45.90.123.131:8080/api/v1',
-  auth: 'http://45.90.123.131:8080/api/v1/auth',
-  locations: 'http://45.90.123.131:8080/api/v1/locations',
-  report: 'http://45.90.123.131:8080/api/v1/report',
-  users: 'http://45.90.123.131:8080/api/v1/users',
-  specialization: 'http://45.90.123.131:8080/api/v1/specialization',
-  patient: 'http://45.90.123.131:8080/api/v1/patient',
-  medicine: 'http://45.90.123.131:8080/api/v1/medicine',
-  history: 'http://45.90.123.131:8080/api/v1/history',
-  upload: 'http://45.90.123.131:8080/api/v1/upload',
-  print: 'http://45.90.123.131:8080/api/v1/print'
+  apiUrl: 'https://45.90.123.131:8080/api/v1',
+  auth: 'https://45.90.123.131:8080/api/v1/auth',
+  locations: 'https://45.90.123.131:8080/api/v1/locations',
+  report: 'https://45.90.123.131:8080/api/v1/report',
+  users: 'https://45.90.123.131:8080/api/v1/users',
+  specialization: 'https://45.90.123.131:8080/api/v1/specialization',
+  patient: 'https://45.90.123.131:8080/api/v1/patient',
+  medicine: 'https://45.90.123.131:8080/api/v1/medicine',
+  history: 'https://45.90.123.131:8080/api/v1/history',
+  upload: 'https://45.90.123.131:8080/api/v1/upload',
+  print: 'https://45.90.123.131:8080/api/v1/print'
 
 };
