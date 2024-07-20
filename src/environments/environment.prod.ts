cdc8fcc786d8b4a77e7540932cb979f6c3433b64
mohamed <moahemd3nab@gmail.com>
2024-07-20 15:36:54 +0300

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://45.90.123.131:8080/api/v1',
-  auth: 'http://45.90.123.131:8080/api/v1/auth',
-  locations: 'http://45.90.123.131:8080/api/v1/locations',
-  report: 'http://45.90.123.131:8080/api/v1/report',
-  users: 'http://45.90.123.131:8080/api/v1/users',
-  specialization: 'http://45.90.123.131:8080/api/v1/specialization',
-  patient: 'http://45.90.123.131:8080/api/v1/patient',
-  medicine: 'http://45.90.123.131:8080/api/v1/medicine',
-  history: 'http://45.90.123.131:8080/api/v1/history',
-  upload: 'http://45.90.123.131:8080/api/v1/upload',
-  print: 'http://45.90.123.131:8080/api/v1/print'
+  apiUrl: 'https://api.nabdelhayah.online/api/v1',
+  auth: 'https://api.nabdelhayah.online/api/v1/auth',
+  locations: 'https://api.nabdelhayah.online/api/v1/locations',
+  report: 'https://api.nabdelhayah.online/api/v1/report',
+  users: 'https://api.nabdelhayah.online/api/v1/users',
+  specialization: 'https://api.nabdelhayah.online/api/v1/specialization',
+  patient: 'https://api.nabdelhayah.online/api/v1/patient',
+  medicine: 'https://api.nabdelhayah.online/api/v1/medicine',
+  history: 'https://api.nabdelhayah.online/api/v1/history',
+  upload: 'https://api.nabdelhayah.online/api/v1/upload',
+  print: 'https://api.nabdelhayah.online/api/v1/print'
 
 };
